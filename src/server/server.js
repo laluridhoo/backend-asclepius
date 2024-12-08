9f969043574edf0a4cbe4ab1e5785375e5dbf2c1
lalu kurnia muhammad ridho <nama_user@DESKTOP-SMPFNDV>
2024-12-08 23:37:38 +0800

    fix bugs host value

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const InputError = require('../exceptions/InputError');
 (async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
               origin: ['*'],
